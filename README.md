a989457d2a8ba4df92cc502857a1d80f62ba2122
Numair <hussainnumair96@gmail.com>
2021-01-11 12:48:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flask-tutorial
+
+Just going through  [Flask's official tutorial](https://flask.palletsprojects.com/en/1.1.x/tutorial/)
+
---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    print(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='abcd',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed
+        app.config.from_mapping(test_config)
+
+    # ensuring the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # simple page for hello world
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
